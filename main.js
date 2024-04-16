caae1a715e150fc7f5da955f5a4dd40da51e5f22
¡Duque <atanaelleonardo@gmail.com>
2024-04-16 18:58:27 -0300

    Remove counter setup and related import
    This commit removes the import of the `counter.js` module and the associated function call to `setupCounter`. The HTML element selection and modification for the counter have also been removed. The game-related

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div class"game">
     <h1>Hello Tic Tac Toe</h1>
   </div>
 `;
-
-setupCounter(document.querySelector("#counter"));
