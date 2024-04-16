5846db5d903cddf5c74df4d3d12f91fa0e067b90
¡Duque <atanaelleonardo@gmail.com>
2024-04-16 19:07:00 -0300

    'Remove counter functionality and import setupCounter from tic-tac-toe module'
    Explanation:
    
    - The `counter.js` file has been deleted, along with its associated counter functionality.
    - The `

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import "./style.css";
+import { setupCounter } from "./tic-tac-toe.js";
 
 document.querySelector("#app").innerHTML = `
   <div class"game">
     <h1>Hello Tic Tac Toe</h1>
   </div>
 `;
+
+setupCounter(document.querySelector("#counter"));
---
+++ tic-tac-toe.js
@@ -0,0 +1,5 @@
+export const tic_tac_toe = {
+  board: ["", "", "", "", "", "", "", "", ""],
+  symbols: ["O", "X"],
+
+};
