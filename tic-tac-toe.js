c4fc6d4a40118374ccef509b8803b3854dd9e2b9
¡Duque <atanaelleonardo@gmail.com>
2024-04-16 19:07:18 -0300

    "Added init method to set container element in tic-tac-toe module"
    This commit adds a new `init` method to the `tic_tac_toe` module, which allows setting the `container

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,11 @@
 export const tic_tac_toe = {
   board: ["", "", "", "", "", "", "", "", ""],
   symbols: ["O", "X"],
+  containerElement: null,
+
+  init: function (container) {
+    this.containerElement = container;
+  }
+
 
 };
